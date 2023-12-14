ee21ab26ea666fec1c142f282e015ebb3e3a03cb
jaowei <jaowei.8@gmail.com>
2023-12-14 22:55:53 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { createSignal } from "solid-js";
+import solidLogo from "./assets/solid.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      <div class="bg-blue-400">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} class="logo" alt="Vite logo" />
+        </a>
+        {/* <a href="https://solidjs.com" target="_blank">
+          <img src={solidLogo} class="logo solid" alt="Solid logo" />
+        </a> */}
+      </div>
+      <h1 class="border-solid border border-black bg-red-200">Vite + Solid</h1>
+      <div class="card border border-red-200">
+        <button bg="red-200" onClick={() => setCount((count) => count + 1)}>
+          count is {count()}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Solid logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
