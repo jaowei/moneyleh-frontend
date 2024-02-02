40e7ed29c0f1272df1ab1714a70fbca3d18f5c99
jaowei <jaowei.8@gmail.com>
2024-02-02 22:31:12 +0800

    remove component

--- src/components/PrimaryButton.tsx
+++
@@ -1,6 +0,0 @@
-import { ParentComponent, splitProps } from "solid-js";
-
-export const PrimaryButton: ParentComponent = (props) => {
-  const [nonAttributes, others] = splitProps(props, ["children"]);
-  return <button {...others}>{nonAttributes.children}</button>;
-};
